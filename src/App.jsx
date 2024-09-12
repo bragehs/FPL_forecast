41514c88104878b5a2a12a002b18fee85b8e428e
bragehs <brage.skjorestad@gmail.com>
2024-09-12 18:54:56 +0200

    endret tekst

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="card">
-      <h1>Velkommen! 👋</h1>
+      <h1>Hei!!!!!! 👋</h1>
       <p>
         Du kan se koden for dette prosjektet{" "}
         <a href="https://github.com/bekk/prosjektutgangspunkt">
