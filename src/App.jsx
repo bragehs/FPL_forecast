3a7f349cfb188279233b2ea2b4f50e26b25af2bc
bragehs <brage.skjorestad@gmail.com>
2024-09-12 20:19:16 +0200

    created button second attempt

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import './App.css'; // Keep your styles
 function Home() {
   return (
     <div className="card">
-      <h1>Cumstink.com 👋</h1>
-      <p>nettsiden til verdens farligste mann</p>
-      {/* Link button to navigate to the new site */}
+      <h1>Cumstink.com 👋
+      </h1>
+        <p>nettsiden til verdens farligste mann
+       </p>
       <Link to="/new-page">
         <button>Go to New Page</button>
       </Link>
@@ -20,9 +21,10 @@ function Home() {
 function NewPage() {
   return (
     <div className="card">
-      <h1>Welcome to the New Page 🎉</h1>
-      <p>This is a completely different page!</p>
-      {/* Button to navigate back to home */}
+      <h1>Welcome to the New Page 🎉
+      </h1>
+      <p>This is a completely different page!
+      </p>
       <Link to="/">
         <button>Back to Home</button>
       </Link>
