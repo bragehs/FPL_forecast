97ab1298d8eff43a6fc1651d8f44f8cda6b0f5ef
bragehs <brage.skjorestad@gmail.com>
2024-09-13 10:06:47 +0200

    endret tekst

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import './App.css'; // Keep your styles
-
-// Component for the home page
-function Home() {
-  return (
-    <div className="card">
-      <h1>Cumstink.com 👋
-      </h1>
-        <p>nettsiden til verdens farligste mann
-       </p>
-      <Link to="/new-page">
-        <button>Go to New Page</button>
-      </Link>
-    </div>
-  );
-}
-
-// Component for the new page with a different header
-function NewPage() {
-  return (
-    <div className="card">
-      <h1>Welcome to the New Page 🎉
-      </h1>
-      <p>This is a completely different page!
-      </p>
-      <Link to="/">
-        <button>Back to Home</button>
-      </Link>
-    </div>
-  );
-}
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new-page" element={<NewPage />} />
-      </Routes>
-    </Router>
+    <div className="card">
+      <h1>Velkommen! 👋</h1>
+      <p>
+        til min første nettside
+      </p>
+    </div>
   );
 }
 
 export default App;
-
