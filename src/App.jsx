ec55b6f1bd66e7af2d008ab5b56773d3939374d3
bragehs <brage.skjorestad@gmail.com>
2024-10-01 13:29:49 +0200

    fetch and parse csv file

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,32 @@ import Papa from "papaparse";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="card">
-      <h1>Velkommen! 👋
-      </h1>
-      <p>
-        til min første nettside!!!!
-      </p>
-    </div>
-  );
+  const [csvData, setCsvData] = useState([]);
+
+  useEffect( () => {
+    const fetchCsvData = async () => {
+      try {
+        //define variables to fetch data from csv and process the data
+        const response = await fetch('/archive/2021-2022.csv'); 
+        const reader = response.body.getReader(); 
+        const decoder = new TextDecoder("utf-8");
+        const result = await reader.read();
+        const csvText = decoder.decode(result.value);
+
+        // parse csv with papaparse
+        Papa.parse(csvText, {
+          header: true,
+          skipEmtpyLines: true,
+          complete: (result) => {
+            setCsvData(result.data);
+          },
+        });
+      } catch (error){
+        console.error("Error fetching the CSV file:", error);
+      }    
+    };
+
+    fetchCsvData();
+}, []);
 }
 
-export default App;
