eb0f7a11556f9c7b6d49d7988b04ce6eb37d2071
bragehs <brage.skjorestad@gmail.com>
2024-10-22 23:55:45 +0200

    dummmy test

--- src/premierleague.jsx
+++ src/premierleague.jsx
@@ -9,17 +9,44 @@ const PremierLeagueTable = () => {
 ];
 
   useEffect(() => {
-    axios.get('https://45ec-88-92-69-129.ngrok-free.app')
-      .then(response => {
-        console.log('Data fetched:', response.data);
-        setData(response.data.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      });
+    const dummyData = [
+      {
+        "Rk": 1,
+        "Squad": "Manchester City",
+        "MP": 38,
+        "W": 29,
+        "D": 6,
+        "L": 3,
+        "Last 5": "WWWWW",
+        "GF": 99,
+        "GA": 28,
+        "GD": 71,
+        "Pts": 93,
+        "xG": 80.5,
+        "xGA": 26.3,
+        "xGD": 54.2,
+        "Pts/MP": 2.45,
+        "Top Team Scorer": "Erling Haaland",
+        "Goalkeeper": "Ederson",
+        "Attendance": "53,000",
+        "Notes": "Champions"
+      }
+    ];
+    setData(dummyData);
+    setLoading(false);
   }, []);
+  //  axios.get('https://45ec-88-92-69-129.ngrok-free.app')
+    //  .then(response => {
+       // console.log('Data fetched:', response.data);
+       // setData(response.data.data);
+     //   setLoading(false);
+   //   })
+    //  .catch(error => {
+    //    console.error('Error fetching data:', error);
+   //     setLoading(false);
+    //  });
+ // }, []);
+
 
   if (loading) {
     return <div>Loading...!!!!</div>;
